eb87e9e3b9db1c83f512ffbe9af5191798d3df39
fbibi4 <127233513+fbibi4@users.noreply.github.com>
2023-09-11 17:21:22 +0100

    added the footer with Github URL

--- src/App.js
+++ src/App.js
@@ -2,5 +2,18 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project was designed by Farha Bibi{" "}
+        <a
+          href="https://github.com/fbibi4/react-weather-project"
+          target="blank"
+        >
+          opened-sourced on Github.
+        </a>
+      </footer>
+    </div>
+  );
 }
