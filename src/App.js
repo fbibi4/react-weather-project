3a65352f32293a67d13709b5d159e18050514253
fbibi4 <127233513+fbibi4@users.noreply.github.com>
2023-09-12 11:33:20 +0100

    Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -4,16 +4,18 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was designed by Farha Bibi{" "}
-        <a
-          href="https://github.com/fbibi4/react-weather-project"
-          target="blank"
-        >
-          opened-sourced on Github.
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was designed by Farha Bibi{" "}
+          <a
+            href="https://github.com/fbibi4/react-weather-project"
+            target="blank"
+          >
+            opened-sourced on Github.
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
