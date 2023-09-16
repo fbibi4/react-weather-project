2fa9a5411595f2044e40d9e060198f18e22d07d3
fbibi4 <127233513+fbibi4@users.noreply.github.com>
2023-09-16 17:01:12 +0100

    added new WeatherForecast component

--- src/WeatherIcon.js
+++ src/WeatherIcon.js
@@ -26,7 +26,7 @@ export default function WeatherIcon(props) {
     <ReactAnimatedWeather
       icon={codeMapping[props.code]}
       color="rgb(21, 115, 71)"
-      size={50}
+      size={props.size}
       animate={true}
     />
   );
--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -16,7 +16,7 @@ export default function WeatherInfo(props) {
       <div className="row mt-3">
         <div className="col-6">
           <div className="d-flex align-items-center">
-            <WeatherIcon code={props.data.icon} />
+            <WeatherIcon code={props.data.icon} size={56} />
             <WeatherTemperature celsius={props.data.temperature} />
           </div>
         </div>
